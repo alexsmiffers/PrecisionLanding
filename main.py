1818319a1622ec185952092aa69234118bef0cc0
alexsmiffers <alexsmiffers@icloud.com>
2025-12-08 14:44:04 +0700

    changes2

--- main.py
+++ main.py
@@ -163,8 +163,6 @@ def aruco(settings, camMatrix, distCoeffs):
 
     # MAVLink connection
     # m = mavutil.mavlink_connection(settings.get('SERIAL_DEV'), baud=settings.get('BAUD'))
-
-    mavutil.set_dialect("ardupilotmega")
     m = mavutil.mavlink_connection('tcp:192.168.10.233:5760', baud=settings.get('BAUD'))
     # Wait for heartbeat so target system/component IDs are known (optional but helpful)
     try:
